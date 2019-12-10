cbb8a81615600cf5c02ab9a3b78887dc8460f101
2190933536@qq.com <2190933536@qq.com>
2019-12-11 00:16:17 +0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:2190933536@qq.com/learning-diary.git master:gh-pages
+git push -f "https://github.com/junelongchan/learning-diary.git" master:gh-pages
 
 cd -
